f3169415329e08b4304cbd86cdcc142de1fdffc9
metacoding <getinthere@naver.com>
2023-05-11 20:26:39 +0900

    fdasf

--- src/main/java/com/example/kakao/products/ProductService.java
+++ src/main/java/com/example/kakao/products/ProductService.java
@@ -1,15 +1,12 @@
 package com.example.kakao.products;
 
 import com.example.kakao._core.errors.exception.Exception404;
-import com.example.kakao.users.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
