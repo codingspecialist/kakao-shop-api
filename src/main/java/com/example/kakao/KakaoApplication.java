2cf1a6c19e1145b29d5110fd8d10c7de8d03bca9
metacoding <getinthere@naver.com>
2023-05-19 18:36:05 +0900

    테스트 직전까지 완료 - API 문서 작성 필요

--- src/main/java/com/example/kakao/KakaoApplication.java
+++ src/main/java/com/example/kakao/KakaoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
@@ -26,6 +27,7 @@ public class KakaoApplication {
 		SpringApplication.run(KakaoApplication.class, args);
 	}
 
+	@Profile("dev")
 	@Bean
 	CommandLineRunner init(UserJPARepository userRepo, ProductJPARepository productRepo, OptionJPARepository optionRepo){
 		return args -> {
--- src/test/java/com/example/kakao/integration/UserRestControllerTest.java
+++ src/test/java/com/example/kakao/integration/UserRestControllerTest.java
@@ -1,14 +1,31 @@
 package com.example.kakao.integration;
 
+import com.example.kakao.user.UserJPARepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
 
+import javax.persistence.EntityManager;
 
+@ActiveProfiles("test")
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @Sql("classpath:db/teardown.sql")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class UserRestControllerTest extends MyRestDoc{
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private UserJPARepository userRepository;
+    @Autowired
+    private EntityManager em;
+
+
 }
