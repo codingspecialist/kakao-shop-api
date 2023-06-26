639bad8dba817efcd61bdc3ce0499531944f88c3
metacoding <getinthere@naver.com>
2023-06-26 12:30:01 +0900

    last

--- src/main/java/com/example/kakao/KakaoApplication.java
+++ src/main/java/com/example/kakao/KakaoApplication.java
@@ -1,21 +1,7 @@
 package com.example.kakao;
 
-import com.example.kakao.option.Option;
-import com.example.kakao.option.OptionJPARepository;
-import com.example.kakao.product.Product;
-import com.example.kakao.product.ProductJPARepository;
-import com.example.kakao.user.User;
-import com.example.kakao.user.UserJPARepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 
 @SpringBootApplication
